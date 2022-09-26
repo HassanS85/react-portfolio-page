4881928701ca4485b744812d38cb43cfabf986b2
Hassan <hassan.seedat07@gmail.com>
2022-09-26 23:06:38 +0100

    added in NavBar

--- react-portfolio-page/src/components/NavBar.js
+++ react-portfolio-page/src/components/NavBar.js
@@ -1,22 +1,53 @@
 import { Navbar, Container } from "react-bootstrap"
+import { useState } from "react";
+
 
 export const NavBar = () => {
+    const [activeLink, useactiveLink] = useState('home');
+    const [scrolled, useScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if(window.scrollY > 50) {
+                useScrolled(true);
+            } else {
+                useScrolled(false)
+            }
+        }
+      
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll)
+    })
+    
+
+
+
     return (
-            <Navbar bg="light" expand="lg">
-        <Container>
-            <Navbar.Brand href="#home">
-                <img src={''} alt="Logo" />
-                React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav">
-                <span className="navBar-toggler-icon"></span>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
+            <Navbar bg="light" expand="lg" className={scrolled ? "scrolled": ""}>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={''} alt="Logo" />
+                    React-Bootstrap
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navBar-toggler-icon"></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#skills">Link</Nav.Link>
+                            <Nav.Link href="#projects">Link</Nav.Link>
+                        </Nav>
+                        <span className="navBar-text">
+                            <div className="social-icon">
+                                <a href='#'><img src={} alt=""/></a>
+                                <a href='#'><img src={} alt=""/></a>
+                                <a href='#'><img src={} alt=""/></a>
+                            </div>
+                            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         )
 }
